98d2d3e44334ecabc26431468bb2e26566a2e17a
sajib689 <jibonraja11@gmail.com>
2024-02-27 21:35:41 +0600

    ddd

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const displayShowDetails = phone => {
     <input type="checkbox" id="my_modal_6" class="modal-toggle" />
     <div class="modal" role="dialog">
       <div class="modal-box">
-        <h3 class="font-bold text-lg">Hello!</h3>
+        <h3 class="font-bold text-lg">${phone.name}</h3>
         <p class="py-4">This modal works with a hidden checkbox!</p>
         <div class="modal-action">
           <label for="my_modal_6" class="btn">Close!</label>
